214c754c1286164fdb5d080e813e652dbde1422a
v-andrusenko <andrusenko.vp1@gmail.com>
2021-03-09 20:17:51 +0200

    update

--- labwork_3_5/Matrix.cs
+++ labwork_3_5/Matrix.cs
@@ -7,85 +7,73 @@ namespace labwork_3_5
 {
     class Matrix
     {
-        private static object locker = new object();
-        private Random rand;
-        const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        static object locker = new object();
+        static Random rand = new Random();
+        const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-        public int Column { get; set; }
-        public bool NextOne { get; set; }
-
-        public Matrix(int column, bool nextOne)
+        public static void CreateString(object x)
         {
-            Column = column;
-            rand = new Random((int)DateTime.Now.Ticks);
-            NextOne = nextOne;
-        }
+            int stringLength = rand.Next(5, Console.WindowHeight - 10);
+            int pozX = (int)x; 
+            int pozY = rand.Next(0, Console.WindowHeight - 2); 
+            int currentY = 0; 
 
-        private char Sign => letters.ToCharArray()[rand.Next(0, 35)];
-
-        public void Moving()
-        {
-            int lenght;
-            int counter;
-
-            while (true)
+            for (int i = 0; i < stringLength; i++)
             {
-                counter = rand.Next(3, 12);
-                lenght = 0;
-                Thread.Sleep(rand.Next(20, 5000));
-                for (int i = 0; i < 40; i++)
+                lock (locker)
                 {
-                    lock (locker)
+                    if (pozY == Console.WindowHeight - 1)
                     {
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.CursorTop = i - lenght;
-
-                        for (int j = i - lenght; j < i; j++)
-                        {
-                            Console.CursorLeft = Column;
-                            Console.WriteLine("---");
-                        }
-
-                        if (lenght < counter)
-                            lenght++;
-                        else if (lenght == counter)
-                            counter = 0;
-
-                        if (NextOne && i < 20 && i > lenght + 2 && (rand.Next(1, 5) == 3))
-                        {
-                            new Thread((new Matrix(Column, false)).Moving).Start();
-                            NextOne = false;
-                        }
-
-                        if (39 - i < lenght)
-                            lenght--;
-
-                        Console.CursorTop = i - lenght + 1;
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-
-                        for (int j = 0; j < lenght - 2; j++)
-                        {
-                            Console.CursorLeft = Column;
-                            Console.WriteLine(Sign);
-                        }
-
-                        if (lenght >= 2)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.CursorLeft = Column;
-                            Console.WriteLine(Sign);
-                        }
-
-                        if (lenght >= 1)
-                        {
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.CursorLeft = Column;
-                            Console.WriteLine(Sign);
-                        }
-
-                        Thread.Sleep(10);
-
+                        pozY = 0;
+                        Console.SetCursorPosition(pozX, pozY);
+                        currentY = pozY + 1;
                     }
+                    else
+                    {
+                        Console.SetCursorPosition(pozX, pozY++);
+                        currentY = pozY + 1;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(Letters.ToCharArray()[rand.Next(0, 35)]);
+
+                    if (currentY > 3 && i >= 2)
+                    {
+                        Console.SetCursorPosition(pozX, currentY - 3);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(Letters.ToCharArray()[rand.Next(0, 35)]);
+
+                        Console.SetCursorPosition(pozX, currentY - 4);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine(Letters.ToCharArray()[rand.Next(0, 35)]);
+                    }
+                    else if (currentY <= 2)
+                    {
+                        Console.SetCursorPosition(pozX, currentY - 4 + Console.WindowHeight);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(Letters.ToCharArray()[rand.Next(0, 35)]);
+
+                        Console.SetCursorPosition(pozX, currentY - 5 + Console.WindowHeight);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine(Letters.ToCharArray()[rand.Next(0, 35)]);
+                    }
+
+                    if (i == stringLength - 1)
+                    {
+                        if (pozY >= stringLength)
+                        {
+                            Console.SetCursorPosition(pozX, pozY - stringLength);
+                            Console.Write(' ');
+                            i--;
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(pozX, Console.WindowHeight - stringLength + pozY);
+                            Console.Write(' ');
+                            i--;
+                        }
+                    }
+                    Thread.Sleep(10);
                 }
             }
         }
--- labwork_3_5/Program.cs
+++ labwork_3_5/Program.cs
@@ -3,20 +3,14 @@ using System.Threading;
 
 namespace labwork_3_5
 {
-    
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize(80, 42);
-
-            Matrix instance;
-
-            for (int i = 0; i < 26; i++)
+            for (int i = 0; i < Console.WindowWidth - 1;)
             {
-                instance = new Matrix(i * 3, true);
-                new Thread(instance.Moving).Start();
+                new Thread(new ParameterizedThreadStart(Matrix.CreateString)).Start((object)i);
+                i += 2;
             }
         }
     }
