1544ff57df6786909d00d2bd9e39ee972318b682
v-andrusenko <andrusenko.vp1@gmail.com>
2021-03-09 18:40:43 +0200

    update

---
+++ labwork_3_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace labwork_3_4
+{
+    class Program
+    {
+        static void Go(object num)
+        {
+            int n = (int)num;
+            if (n <= 0)
+                return;
+            Thread t = new Thread(Go);
+            t.Name = "Поток " + n;
+            t.Start(n - 1);
+            Console.WriteLine(t.Name);
+        }
+        private static void Main(string[] args)
+        {
+            Go(20);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ labwork_3_5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace labwork_3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
